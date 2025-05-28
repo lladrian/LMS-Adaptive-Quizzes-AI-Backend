56bcb6ad1a15d852c40a8ed438ef76f53bf5f6e2
Adrian C. Manatad <89061206+lladrian@users.noreply.github.com>
2025-05-28 13:57:33 +0800

    updated

--- src/routes/answer_quiz_routes.js
+++ src/routes/answer_quiz_routes.js
@@ -3,10 +3,10 @@ import * as AnswerController from '../controllers/answer_quiz_controller.js'; //
 
 const answerQuizRoutes = Router();
 
-answerQuizRoutes.get('/get_all_answer_specific_exam/:exam_id', AnswerController.get_all_answer_specific_exam);
+answerQuizRoutes.get('/get_all_answer_specific_quiz/:quiz_id', AnswerController.get_all_answer_specific_quiz);
 answerQuizRoutes.get('/get_specific_answer/:answer_id', AnswerController.get_specific_answer);
 answerQuizRoutes.post('/add_answer/:exam_id/:student_id', AnswerController.create_answer);
-answerQuizRoutes.post('/take_exam/:exam_id/:student_id', AnswerController.take_exam);
+answerQuizRoutes.post('/take_exam/:exam_id/:student_id', AnswerController.take_quiz);
 
 
 export default answerQuizRoutes;
